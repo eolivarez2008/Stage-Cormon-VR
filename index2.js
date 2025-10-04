58f3a7d017e068075b1e2dd08b901fcb66195cf4
Emilien Olivarez <eolivarez2008@gmail.com>
2025-10-04 15:48:08 +0200

    Ajustement

--- index2.js
+++ index2.js
@@ -9,7 +9,7 @@ AFRAME.registerComponent("hdr-env", {
     const scene = this.el.sceneEl.object3D;
 
     const loader = new THREE.TextureLoader();
-    loader.setPath("/background/");
+    loader.setPath("./background/");
     loader.load("salle.jpg", (texture) => {
       texture.mapping = THREE.EquirectangularReflectionMapping;
       scene.background = texture;
